da0c0647aad24b788baefa15471786b19d75c2d4
Adeyeye George <manomitehq@gnmail.com>
2022-09-09 10:04:32 -0700

    fix

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,17 +8,25 @@
  */
 int main(void)
 {
-	int num;
+	int digit1;
+	int digit2;
 
-	for (num = 0; num < 90; num++)
+	for (digit1 = 0; digit1 < 9; digit1++)
 	{
-		putchar((num / 10) + '0');
-		putchar((num % 10) + '0');
-		if (num != 89){
+		for (digit2 = digit1 + 1; digit2 < 10; digit2++)
+		{
+			putchar((digit1 % 10) + '0');
+			putchar((digit2 % 10) + '0');
+
+			if (digit1 == 8 && digit2 == 9)
+				continue;
+			
 			putchar(',');
 			putchar(' ');
 		}
 	}
+
 	putchar('\n');
+
 	return (0);
 }
