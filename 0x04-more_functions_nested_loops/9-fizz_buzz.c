00ce601d6ab365446309ac1bfe842840ccbb0af7
Adeyeye George <manomitehq@gnmail.com>
2022-09-15 09:37:22 -0700

    fix

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -6,29 +6,27 @@
  */
 int main(void)
 {
-	int n;
-	
-	n = 1;
-	printf("%d", n);
-	for (n = 2; n <= 100; n++)
+
+	int i;
+
+	for (i = 1; i <= 100; i++)
 	{
-		if ((n % 3 == 0) && (n % 5 == 0))
+		if (i % 15 == 0)
 		{
-			printf(" FizzBuzz");
+			printf("FizzBuzz ");
 		}
-		else if (n % 3 == 0)
+		else if ((i % 3) == 0)
 		{
-			printf(" Fizz");
+			printf("Fizz ");
 		}
-		else if (n % 5 == 0)
+		else if ((i % 5) == 0)
 		{
-			printf(" Buzz");
+			printf("Buzz ");
 		}
 		else
-		{
-			printf(" %d", n)
+		{ 
+			printf("%d ", i);
 		}
-
 	}
 	printf("\n");
 	return (0);
